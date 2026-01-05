37f29044779e016f3e467018e0e836447d79f251
belva pranama sriwibowo <zerobocilicung393@gmail.com>
2026-01-06 06:26:13 +0800

    build(js): import theme builder scripts in main entry point

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import Alpine from 'alpinejs';
+import initTheme from './theme-switch';
+import initThemeBuilder from './theme-builder';
+
+// Initialize Utils (Expose global functions)
+initTheme();
+initThemeBuilder();
+
+// Start Alpine
+window.Alpine = Alpine;
+Alpine.start();
